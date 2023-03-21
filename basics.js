1f0e2d02870ec06431b8802e5d7e4344147a0982
David Radjai <davidradjai@Davids-MacBook-Air.local>
2023-03-21 12:00:08 -0400

    just learned ternary

---
+++ basics.js
@@ -0,0 +1,17 @@
+//command C = > copy command copycopycopy 
+
+
+const a = 5;
+const b = 4;
+const c = a * b;
+
+console.log(c);
+
+const subtotal = 159.87;
+const taxRate = 0.07;
+const taxTotal = subtotal * taxRate;
+const total = subtotal + taxTotal;
+
+
+console.log('Total: ', total);
+console.log(subtotal, taxTotal, total);
